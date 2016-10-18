f0cbb76de0e22fd560666576fcddd93b66cd1102
Kronarq <kronarq@gmail.com>
2016-10-17 22:09:40 -0700

    Add missing require in boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'rails/commands/server'
 
 module Rails  
   class Server
